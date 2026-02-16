1a39116399d08e9ea247401173e04e432ce0a988
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh[D[D[D[C[C[B <paulosagie15>
2026-02-16 09:24:36 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  resources :posts
   devise_for :users
+
+
   root to: "pages#home"
 
   get "About", to: "pages#About"
    get "started", to: "pages#started"
+    get "curriculum", to: "pages#curriculum"
+   get "courses_path", to: "pages#courses_path"
+   get "/software-development-training-london",
+      to: "pages#london"
+ get "/software-development-training-london",
+      to: "pages#london"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
